de9724b078d93805a840fd3ac8e77e9089d56d8d
taku8a <takuya0505hsmt@gmail.com>
2021-10-26 14:11:52 +0000

    [Add]usersコントローラーを変更

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   # get 'projects/destroy'
   # get 'homes/top'
   # get 'homes/about'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
