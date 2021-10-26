268da0774ae5f408435cc25fc7d1e1d598395e18
taku8a <takuya0505hsmt@gmail.com>
2021-10-26 16:36:37 +0000

    [Add]bootstrap

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,22 +1,53 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  
   def index
+    @projects = Project.all
   end
 
   def show
+    @project = Project.find(params[:id])
   end
 
   def edit
   end
 
   def new
+    @project = Project.new
   end
 
   def create
+    @project = Project.new(project_params)
+    @project.owner_id = current_user.id
+    if @project.save
+      redirect_to projects_path
+    else
+      render "new"
+    end
   end
 
   def update
+    if @project.update(project_params)
+      redirect_to projects_path
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :introduction, :project_image)
+  end
+
+  def ensure_correct_user
+    @project = Project.find(params[:id])
+    unless @project.owner_id == current_user.id
+      projects_path
+    end
   end
 end
