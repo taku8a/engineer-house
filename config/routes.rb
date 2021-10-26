eb29d2100d40b8ce43cd35331c028d725210a84a
taku8a <takuya0505hsmt@gmail.com>
2021-10-26 16:10:03 +0000

    [Add]routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,23 @@ Rails.application.routes.draw do
   # get 'projects/destroy'
   # get 'homes/top'
   # get 'homes/about'
-  devise_for :users, controllers: {
-    registrations: "users/registrations",
-    sessions: "users/sessions"
-  }
+
+  # devise_for :users, controllers: {
+  #   registrations: "users/registrations",
+  #   sessions: "users/sessions"
+  # }
+  devise_for :users, skip: :all
+  devise_scope :user do
+    get '/sign_in' => 'users/sessions#new', as: :new_user_session
+    post '/sign_in' => 'users/sessions#create', as: :user_session
+    delete '/sign_out' => 'users/sessions#destroy', as: :destroy_user_session
+    get '/sign_up' => 'users/registrations#new', as: :new_user_registration
+    post '/sign_up' => 'users/registrations#create', as: :user_registration
+  end
+  
+  root :to => "homes#top"
+  get "/about" => "homes#about"
+  resources :projects
+  resource :project_chat, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
