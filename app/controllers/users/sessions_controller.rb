4bff2921dc521764e01e2ee54c5a1ba2a82a04e2
taku8a <takuya0505hsmt@gmail.com>
2021-10-29 16:52:06 +0000

    [Fix]マイページ作成

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -22,11 +22,11 @@ class Users::SessionsController < Devise::SessionsController
   protected
 
   def after_sign_in_path_for(resource)
-    root_path
+    mypage_users_path
   end
 
   def after_sign_out_path_for(resource)
-    new_user_session_path
+    root_path
   end
 
   def user_state
