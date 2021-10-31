4a3efd7f5fc47eb4712370487b04f05fbcc15762
taku8a <takuya0505hsmt@gmail.com>
2021-10-31 07:07:34 +0000

    [Add]ジャバスクリプト

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,9 @@ class User < ApplicationRecord
   has_many :projects, through: :project_members
 
   attachment :profile_image
-  
+
   validates :name, presence: true
   validates :is_valid, inclusion: [true, false]
-  
+  validates :email, presence: true
+  validates :password, presence: true
 end
