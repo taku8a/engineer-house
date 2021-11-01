0b4f4c68e4effffad7f479b35f10f166da039e86
taku8a <takuya0505hsmt@gmail.com>
2021-11-01 04:38:34 +0000

    [Add]投稿関連

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,22 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user!, only: [:edit, :update, :destroy]
-  
+
   def index
     @posts = Post.page(params[:page]).reverse_order
   end
-  
+
   def show
     @post = Post.find(params[:id])
   end
-  
+
   def edit
   end
-  
+
   def new
     @post = Post.new
   end
-  
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
       render "new"
     end
   end
-  
+
   def update
    if @post.update(post_params)
      redirect_to posts_path
@@ -33,23 +33,23 @@ class PostsController < ApplicationController
      render "edit"
    end
   end
-  
+
   def destroy
     @post.destroy
     redirect_to posts_path
   end
-  
+
   private
-  
+
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, genre_ids: [])
   end
-  
+
   def ensure_correct_user!
     @post = Post.find(params[:id])
     unless @post.user_id = current_user.id
       posts_path
     end
   end
-  
+
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,4 @@
 class Genre < ApplicationRecord
   has_many :post_genres, dependent: :destroy
+  has_many :posts, through: :post_genres
 end
