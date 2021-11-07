454b2f5c6ebd0200f5132f905cbf1d8157335d40
taku8a <takuya0505hsmt@gmail.com>
2021-11-07 11:49:50 +0000

    [Fix]コントローラー修正

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,19 +1,20 @@
 class PostCommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post_comment!
-  
+  before_action :ensure_correct_user!, only: [:edit, :update, :destroy]
+
   def index
     @post_comments = PostComment.page(params[:page]).reverse_order
   end
-  
+
   def show
     @post_comment = PostComment.find(params[:id])
   end
-  
+
   def new
     @post_comment = PostComment.new
   end
-  
+
   def create
     @post_comment =PostComment.new(post_comment_params)
     if @post_comment.save
@@ -22,34 +23,38 @@ class PostCommentsController < ApplicationController
       render "new"
     end
   end
-  
+
   def edit
-    @post_comment = PostComment.find(params[:id])
   end
-  
+
   def update
-    @post_comment = PostComment.find(params[:id])
     if @post_comment.update(post_comment_params)
-      redirect_to post_post_comments_path(@post), notice: t("notice.post_update")
+      redirect_to post_post_comment_path(@post.id,@post_comment.id), notice: t("notice.post_update")
     else
       render "edit"
     end
   end
-  
+
   def destroy
-    @post_comment = PostComment.find(params[:id])
     @post_comment.destroy
     redirect_to post_post_comments_path(@post), notice: t("notice.post_destroy")
   end
-  
+
   private
-  
+
   def post_comment_params
     params.require(:post_comment).permit(:comment, :user_id, :post_id, genre_detail_ids: [])
   end
-  
+
   def set_post_comment!
     @post = Post.find(params[:post_id])
   end
-  
+
+  def ensure_correct_user!
+    @post_comment = PostComment.find(params[:id])
+    unless @post_comment.user_id == current_user.id
+      redirect_to post_post_comment_path(@post.id, @post_comment.id), alert: t("alert.owner_right")
+    end
+  end
+
 end
