3b4f3a4547e055c9f8dcdae44458d7b5aa1fd8db
taku8a <takuya0505hsmt@gmail.com>
2021-11-09 02:20:05 +0000

    [Fix]マイページpagination変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,16 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @projects = current_user.projects.page(params[:page]).reverse_order
-    @posts = current_user.posts.page(params[:page]).reverse_order
+    @projects = current_user.projects.page(params[:project_page]).reverse_order
+    @posts = current_user.posts.page(params[:post_page]).reverse_order
     @my_projects = []
     @projects.each do |project|
       if project.owner_id == current_user.id
         @my_projects << project
       end
     end
-    @my_project = Kaminari.paginate_array(@my_projects).page(params[:page]).per(10)
-    @post_comments = current_user.post_comments.page(params[:page]).reverse_order
+    @my_project = Kaminari.paginate_array(@my_projects).page(params[:my_project_page]).per(10)
+    @post_comments = current_user.post_comments.page(params[:comment_page]).reverse_order
     @my_genres = []
     @genres = Genre.all
     @genres.each do |genre|
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
         @my_genres << genre
       end
     end
-    @my_genre = Kaminari.paginate_array(@my_genres).page(params[:page]).per(10)
+    @my_genre = Kaminari.paginate_array(@my_genres).page(params[:genre_page]).per(10)
   end
 
   def edit
