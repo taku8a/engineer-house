9413e99f33119b4bab55326a6e67e0311fd47f04
taku8a <takuya0505hsmt@gmail.com>
2021-11-15 15:05:29 +0000

    [Fix]新規登録・ログイン画面修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,14 +72,3 @@ Rails.application.configure do
 
   
 end
-
-# config.action_mailer.delivery_method = :smtp
-#   config.action_mailer.smtp_settings = {
-#   address:              'smtp.gmail.com',
-#   port:                  587,
-#   domain:               'gmail.com',
-#   user_name:             ENV['SEND_MAIL'],
-#   password:              ENV['SEND_MAIL_PASSWORD'],
-#   authentication:       'plain',
-#   enable_starttls_auto:  true
-# }
