c84858453b8384f16261be207faa58516637ac2d
taku8a <takuya0505hsmt@gmail.com>
2021-11-17 16:32:19 +0000

    [Fix]usersコントローラーparams変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,7 +67,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image_id, :introduction)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
 end
