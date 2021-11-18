9bfe0847ab24556c64590f5d6ac919106fc1a1df
taku8a <takuya0505hsmt@gmail.com>
2021-11-18 02:01:58 +0000

    [Fix]email編集機能修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -46,7 +46,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile_image, :is_valid, :introduction])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile_image, :is_valid, :introduction, :email])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
