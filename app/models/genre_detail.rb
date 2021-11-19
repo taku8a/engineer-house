fe7f07dc27c4cc5c180ac3708114adc1fc8c5ddd
taku8a <takuya0505hsmt@gmail.com>
2021-11-19 02:36:53 +0000

    [Fix]メソッド化追加

--- app/models/genre_detail.rb
+++ app/models/genre_detail.rb
@@ -4,7 +4,19 @@ class GenreDetail < ApplicationRecord
   has_many :posts, through: :post_genre_details
   has_many :post_comment_genre_details, dependent: :destroy
   has_many :post_comments, through: :post_comment_genre_details
-  
+
   validates :title, presence: true
   validates :body, presence: true
+
+  def update_time
+    updated_at.strftime("%Y/%m/%d %H:%M")
+  end
+
+  def short_title
+    title.truncate(10)
+  end
+
+  def short_body
+    body.truncate(10)
+  end
 end
