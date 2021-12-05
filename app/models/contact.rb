25613342d4a774568d18875453fc0918a10b3a28
taku8a <takuya0505hsmt@gmail.com>
2021-12-05 03:23:53 +0000

    [Fix]メール画面修正

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,4 +4,20 @@ class Contact < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }}
   validates :content, presence: true
+  
+  def highly
+    self.score >= 0.8
+  end
+  
+  def little_good
+    self.score < 0.8 && self.score >= 0.2
+  end
+  
+  def moderately
+    self.score < 0.2 && self.score > -0.2
+  end
+  
+  def little_bad
+    self.score <= -0.2 && self.score > -0.8
+  end
 end
