457bc936bd3a738896b5f78815bd355bf31de06f
taku8a <takuya0505hsmt@gmail.com>
2021-12-16 01:52:05 +0000

    [Fix]ジャンル一覧・新規投稿画面のテスト

--- spec/system/02_after_logins_spec.rb
+++ spec/system/02_after_logins_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe "[STEP2]ユーザーログイン後のテスト", type: :system d
   let!(:other_post) { create(:post, user: other_user) }
   let!(:post_comment) { create(:post_comment, user: user, post: post) }
   let!(:other_post_comment) { create(:post_comment, user: other_user, post: post) }
+  let!(:genre) { create(:genre) }
+  let!(:other_genre) { create(:genre) }
 
   # なぜ、let(:post) { create(:post) }ではダメなのか？　association :userが効いているから良いのでは？
   # →ログインユーザーの投稿ではない為。association :userはpostデータ作成時にuserデータも生成する。つまり、ここでは、ログインユーザー
@@ -428,6 +430,55 @@ RSpec.describe "[STEP2]ユーザーログイン後のテスト", type: :system d
       end
       it 'リダイレクト先が、保存できた投稿の詳細画面になっている' do
         expect(current_path).to eq post_post_comment_path(post_comment.post_id, post_comment.id)
+      end
+    end
+  end
+
+   describe 'ジャンル一覧・新規登録画面のテスト' do
+    before do
+      visit genres_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq genres_path
+      end
+      it '自分のジャンルと他人のジャンルの投稿数のリンク先がそれぞれ正しい' do
+        expect(page).to have_link genre.posts.count, href: genre_path(genre)
+        expect(page).to have_link other_genre.posts.count, href: genre_path(other_genre)
+      end
+      it '自分のジャンル名と他人のジャンル名がそれぞれ表示される' do
+        expect(page).to have_content genre.short_name
+        expect(page).to have_content other_genre.short_name
+      end
+      it '「ジャンル一覧・追加」と表示される' do
+        expect(page).to have_content 'ジャンル一覧・追加'
+      end
+      it '「ジャンル」と表示される' do
+        expect(page).to have_content 'ジャンル'
+      end
+      it '「投稿数」と表示される' do
+        expect(page).to have_content '投稿数'
+      end
+      it '新規登録ボタンが表示される' do
+        expect(page).to have_button '新規登録'
+      end
+      it 'name登録フォームが表示される' do
+        expect(page).to have_field 'genre[name]'
+      end
+    end
+
+    context 'ジャンル登録成功テスト', js: true do
+      before do
+        fill_in 'genre[name]', with: Faker::Lorem.characters(number: 10)
+      end
+
+      it '自分の新しいジャンルが正しく保存される' do
+        expect { click_button '新規登録' }.to change{ Genre.count }.by(1), xhr: true
+      end
+      it 'リダイレクト先が、保存できた投稿の詳細画面になっている' do
+        click_button '新規登録'
+        expect(current_path).to eq genre_path(Genre.last.id), xhr: true
       end
     end
   end
