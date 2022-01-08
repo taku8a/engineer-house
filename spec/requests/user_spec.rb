a6500e5436ef00c4cac4814f3fb5e892c2bc1cad
taku8a <takuya0505hsmt@gmail.com>
2022-01-08 03:33:39 +0000

    [Fix]users_controllerテスト修正

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,12 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe "ユーザーログイン後のコントローラーテスト", type: :request do
+RSpec.describe "users_controller ", type: :request do
   describe 'GET /index' do
-    # @user = create(:user)
-    it 'リクエストが成功すること' do
-      @user = create(:user)
-      get '/index'
-      expect(response.status).to eq 200
+    context 'as a user not to login' do
+      it 'returns a 302 response' do
+        get index_users_path
+        expect(response.status).to eq 302
+      end
+      it 'redirect to the sign-in page' do
+        get index_users_path
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'as an authenticated user' do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+      
+      it 'respond successfully' do
+        sign_in @user
+        get index_users_path
+        expect(response).to be_successful
+      end
+      it 'returns a 200 response' do
+        sign_in @user
+        get index_users_path
+        expect(response.status).to eq 200
+      end
     end
   end
 end
