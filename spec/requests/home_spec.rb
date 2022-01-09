af76f6f62e1378284dcf10305b77de3f43ee2e87
taku8a <takuya0505hsmt@gmail.com>
2022-01-09 02:48:09 +0000

    [Add]GET users_controller showアクションテスト

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "homes_controllerのテスト", type: :request do
-  describe "GET /" do
+RSpec.describe "homes_controllerテスト", type: :request do
+  describe "GET topアクションテスト" do
     it 'リクエストが成功すること' do
       get root_path
       expect(response.status).to eq 200
     end
   end
 
-  describe 'GET /about' do
+  describe 'GET aboutアクションテスト' do
     it 'リクエストが成功すること' do
       get about_path
       expect(response.status).to eq 200
--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,31 +1,63 @@
 require 'rails_helper'
 
-RSpec.describe "users_controller ", type: :request do
-  describe 'GET /index' do
-    context 'as a user not to login' do
-      it 'returns a 302 response' do
+RSpec.describe "users_controllerテスト", type: :request do
+  describe 'GET indexアクションテスト' do
+    context 'ユーザーがログインしていない時' do
+      it '302レスポンスが返る' do
         get index_users_path
         expect(response.status).to eq 302
       end
-      it 'redirect to the sign-in page' do
+      it 'ログイン画面にリダイレクトされる' do
         get index_users_path
         expect(response).to redirect_to new_user_session_path
       end
     end
 
-    context 'as an authenticated user' do
+    context 'ユーザーがログインしているとき' do
       before do
         @user = FactoryBot.create(:user)
       end
-      
-      it 'respond successfully' do
+
+      it '正常に応答する' do
         sign_in @user
         get index_users_path
         expect(response).to be_successful
       end
-      it 'returns a 200 response' do
+      it '200レスポンスが返る' do
         sign_in @user
         get index_users_path
+        expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe 'GET showアクションテスト' do
+    before do
+      @user = FactoryBot.create(:user)
+    end
+
+    context 'ユーザーがログインしていない時' do
+      it '302レスポンスが返る' do
+        get show_users_path(@user)
+        expect(response.status).to eq 302
+      end
+      it 'ログイン画面にリダイレクトされる' do
+        get show_users_path(@user)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ユーザーがログインしているとき' do
+      before do
+        sign_in @user
+      end
+
+      it '正常に応答する' do
+        get show_users_path(@user)
+        expect(response).to be_successful
+      end
+      it '200レスポンスが返る' do
+        get show_users_path(@user)
         expect(response.status).to eq 200
       end
     end
