db0b3ba4086d67a1dd3628db985528c18bc2cb34
taku8a <takuya0505hsmt@gmail.com>
2022-01-11 02:06:27 +0000

    [Add]users_controllerのunsubscribeテスト

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -90,4 +90,62 @@ RSpec.describe "users_controllerテスト", type: :request do
       end
     end
   end
+
+  describe 'GET editアクションテスト' do
+    context 'ユーザーがログインしていない時' do
+      it '302レスポンスが返る' do
+        get edit_users_path
+        expect(response.status).to eq 302
+      end
+      it 'ログイン画面にリダイレクトされる' do
+        get edit_users_path
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ユーザーがログインしているとき' do
+      before do
+        @user = FactoryBot.create(:user)
+        sign_in @user
+      end
+
+      it '正常に応答する' do
+        get edit_users_path
+        expect(response).to be_successful
+      end
+      it '200レスポンスが返る' do
+        get edit_users_path
+        expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe 'GET unsubscribeアクションテスト' do
+    context 'ユーザーがログインしていない時' do
+      it '302レスポンスが返る' do
+        get unsubscribe_users_path
+        expect(response.status).to eq 302
+      end
+      it 'ログイン画面にリダイレクトされる' do
+        get unsubscribe_users_path
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ユーザーがログインしているとき' do
+      before do
+        @user = FactoryBot.create(:user)
+        sign_in @user
+      end
+
+      it '正常に応答する' do
+        get unsubscribe_users_path
+        expect(response).to be_successful
+      end
+      it '200レスポンスが返る' do
+        get unsubscribe_users_path
+        expect(response.status).to eq 200
+      end
+    end
+  end
 end
