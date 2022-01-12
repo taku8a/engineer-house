dd135edbc71efb2c4ccd496fe9fcdcf011a7e6d0
taku8a <takuya0505hsmt@gmail.com>
2022-01-12 00:50:17 +0000

    [Add]mypageアクションテスト

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -91,35 +91,6 @@ RSpec.describe "users_controllerテスト", type: :request do
     end
   end
 
-  describe 'GET editアクションテスト' do
-    context 'ユーザーがログインしていない時' do
-      it '302レスポンスが返る' do
-        get edit_users_path
-        expect(response.status).to eq 302
-      end
-      it 'ログイン画面にリダイレクトされる' do
-        get edit_users_path
-        expect(response).to redirect_to new_user_session_path
-      end
-    end
-
-    context 'ユーザーがログインしているとき' do
-      before do
-        @user = FactoryBot.create(:user)
-        sign_in @user
-      end
-
-      it '正常に応答する' do
-        get edit_users_path
-        expect(response).to be_successful
-      end
-      it '200レスポンスが返る' do
-        get edit_users_path
-        expect(response.status).to eq 200
-      end
-    end
-  end
-
   describe 'GET unsubscribeアクションテスト' do
     context 'ユーザーがログインしていない時' do
       it '302レスポンスが返る' do
@@ -144,6 +115,35 @@ RSpec.describe "users_controllerテスト", type: :request do
       end
       it '200レスポンスが返る' do
         get unsubscribe_users_path
+        expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe 'GET mypageアクションテスト' do
+    context 'ユーザーがログインしていない時' do
+      it '302レスポンスが返る' do
+        get mypage_users_path
+        expect(response.status).to eq 302
+      end
+      it 'ログイン画面にリダイレクトされる' do
+        get mypage_users_path
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ユーザーがログインしているとき' do
+      before do
+        @user = FactoryBot.create(:user)
+        sign_in @user
+      end
+
+      it '正常に応答する' do
+        get mypage_users_path
+        expect(response).to be_successful
+      end
+      it '200レスポンスが返る' do
+        get mypage_users_path
         expect(response.status).to eq 200
       end
     end
