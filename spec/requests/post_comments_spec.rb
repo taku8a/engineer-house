2c3f521171eb56d9ea05e8927db481b3859090b5
taku8a <takuya0505hsmt@gmail.com>
2022-01-24 01:20:48 +0000

    [Add]post_comments_controllerのindexアクションテスト

--- spec/requests/post_comments_spec.rb
+++ spec/requests/post_comments_spec.rb
@@ -1,10 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe "PostComments", type: :request do
-  describe "GET /post_comments" do
-    it "works! (now write some real specs)" do
-      get post_comments_index_path
-      expect(response).to have_http_status(200)
+RSpec.describe "posts_comment_controllerのテスト", type: :request do
+  before do
+    @user = FactoryBot.create(:user)
+    @post = FactoryBot.create(:post, user:@user)
+    @post_comment = FactoryBot.create(:post_comment, user:@user, post:@post)
+  end
+
+  describe "GET indexアクションテスト" do
+    context 'ユーザーがログインしていない時' do
+      it '302レスポンスが返る' do
+        get post_post_comments_path(@post_comment.post_id)
+        expect(response.status).to eq 302
+      end
+      it 'ログイン画面にリダイレクトされる' do
+        get post_post_comments_path(@post_comment.post_id)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ユーザーがログインしているとき' do
+      before do
+        sign_in @user
+      end
+
+      it '正常に応答する' do
+        get post_post_comments_path(@post_comment.post_id)
+        expect(response).to be_successful
+      end
+      it '200レスポンスが返る' do
+        get post_post_comments_path(@post_comment.post_id)
+        expect(response.status).to eq 200
+      end
     end
   end
 end
