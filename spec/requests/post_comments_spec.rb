3615d628eaad0bbbb502fca337ad5b1f4bab57c3
taku8a <takuya0505hsmt@gmail.com>
2022-01-25 03:31:38 +0000

    [Add]post_comments_controllerのテスト

--- spec/requests/post_comments_spec.rb
+++ spec/requests/post_comments_spec.rb
@@ -34,4 +34,33 @@ RSpec.describe "posts_comment_controllerのテスト", type: :request do
       end
     end
   end
+
+  describe 'GET showアクションテスト' do
+
+    context 'ユーザーがログインしていない時' do
+      it '302レスポンスが返る' do
+        get post_post_comment_path(@post_comment.post_id,@post_comment.id)
+        expect(response.status).to eq 302
+      end
+      it 'ログイン画面にリダイレクトされる' do
+        get post_post_comment_path(@post_comment.post_id,@post_comment.id)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ユーザーがログインしているとき' do
+      before do
+        sign_in @user
+      end
+
+      it '正常に応答する' do
+        get post_post_comment_path(@post_comment.post_id,@post_comment.id)
+        expect(response).to be_successful
+      end
+      it '200レスポンスが返る' do
+        get post_post_comment_path(@post_comment.post_id,@post_comment.id)
+        expect(response.status).to eq 200
+      end
+    end
+  end
 end
