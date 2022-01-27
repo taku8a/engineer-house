aa1388a91bc7dea99994df8dfac42054d0919a05
taku8a <takuya0505hsmt@gmail.com>
2022-01-27 01:38:18 +0000

    [Add]post_comments_controllerのupdateアクションテスト

--- spec/requests/post_comments_spec.rb
+++ spec/requests/post_comments_spec.rb
@@ -91,4 +91,32 @@ RSpec.describe "posts_comment_controllerのテスト", type: :request do
       end
     end
   end
+
+  describe 'PATCH updateアクションテスト' do
+    context 'ユーザーがログインしていない時' do
+      it '302レスポンスが返る' do
+        patch post_post_comment_path(@post_comment.post_id,@post_comment.id)
+        expect(response.status).to eq 302
+      end
+      it 'ログイン画面にリダイレクトされる' do
+        patch post_post_comment_path(@post_comment.post_id,@post_comment.id)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ユーザーがログインしているとき' do
+      before do
+        sign_in @user
+      end
+
+      it 'マイ投稿を更新できる' do
+        post_comment_params = FactoryBot.attributes_for(:post_comment, user: @user,comment: Faker::Lorem.characters(number: 20))
+        patch post_post_comment_path(@post_comment.post_id,@post_comment.id),params: { id: @post_comment.id,post_comment: post_comment_params }
+        get post_post_comment_path(@post_comment.post_id,@post_comment.id)
+        expect(:notice).to be_present
+        expect(response).to be_successful
+        expect(response.status).to eq 200
+      end
+    end
+  end
 end
