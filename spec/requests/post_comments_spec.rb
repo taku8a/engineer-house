3679d516060b08f4a22ad050c4538ee648679694
taku8a <takuya0505hsmt@gmail.com>
2022-01-29 00:53:46 +0000

    [Add]post_comments_controllerのcreateアクションテスト

--- spec/requests/post_comments_spec.rb
+++ spec/requests/post_comments_spec.rb
@@ -147,4 +147,32 @@ RSpec.describe "posts_comment_controllerのテスト", type: :request do
       end
     end
   end
+
+  describe 'POST createアクションテスト' do
+    context 'ユーザーがログインしていない時' do
+      it '302レスポンスが返る' do
+        post post_post_comments_path(@post)
+        expect(response.status).to eq 302
+      end
+      it 'ログイン画面にリダイレクトされる' do
+        post post_post_comments_path(@post)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ユーザーがログインしているとき' do
+      before do
+        sign_in @user
+      end
+
+      it '新規投稿する' do
+        post_comment_params = FactoryBot.attributes_for(:post_comment, user: @user)
+        expect post post_post_comments_path(@post), params: { post_comment: post_comment_params }
+        get post_post_comment_path(@post_comment.post_id,@post_comment.id)
+        expect(:notice).to be_present
+        expect(response).to be_successful
+        expect(response.status).to eq 200
+      end
+    end
+  end
 end
