6f184171ec0dec035b65e9c4fa368d36ae238d5f
taku8a <takuya0505hsmt@gmail.com>
2022-02-02 01:28:53 +0000

    [Add]genres_controllerのshowアクションテスト

--- spec/requests/genres_spec.rb
+++ spec/requests/genres_spec.rb
@@ -34,4 +34,33 @@ RSpec.describe "genre_controllerのテスト", type: :request do
       end
     end
   end
+
+  describe 'GET showアクションテスト' do
+
+    context 'ユーザーがログインしていない時' do
+      it '302レスポンスが返る' do
+        get genre_path(@genre)
+        expect(response.status).to eq 302
+      end
+      it 'ログイン画面にリダイレクトされる' do
+        get genre_path(@genre)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ユーザーがログインしているとき' do
+      before do
+        sign_in @user
+      end
+
+      it '正常に応答する' do
+        get genre_path(@genre)
+        expect(response).to be_successful
+      end
+      it '200レスポンスが返る' do
+        get genre_path(@genre)
+        expect(response.status).to eq 200
+      end
+    end
+  end
 end
