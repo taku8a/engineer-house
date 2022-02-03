15cb479c4c0ec9d3d83d932bd1a2ffdfbee4df7e
taku8a <takuya0505hsmt@gmail.com>
2022-02-03 03:35:11 +0000

    [Add]genres_controllerのeditアクションテスト

--- spec/requests/genres_spec.rb
+++ spec/requests/genres_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "genre_controllerのテスト", type: :request do
   before do
     @user = FactoryBot.create(:user)
     @post = FactoryBot.create(:post, user:@user)
-    @genre = FactoryBot.create(:genre)
+    @genre = FactoryBot.create(:genre, owner_id: @user.id)
   end
 
   describe "GET indexアクションテスト" do
@@ -59,6 +59,34 @@ RSpec.describe "genre_controllerのテスト", type: :request do
       end
       it '200レスポンスが返る' do
         get genre_path(@genre)
+        expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe 'GET editアクションテスト' do
+    context 'ユーザーがログインしていない時' do
+      it '302レスポンスが返る' do
+        get edit_genre_path(@genre)
+        expect(response.status).to eq 302
+      end
+      it 'ログイン画面にリダイレクトされる' do
+        get edit_genre_path(@genre)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ユーザーがログインしているとき' do
+      before do
+        sign_in @user
+      end
+
+      it '正常に応答する' do
+        get edit_genre_path(@genre)
+        expect(response).to be_successful
+      end
+      it '200レスポンスが返る' do
+        get edit_genre_path(@genre)
         expect(response.status).to eq 200
       end
     end
