6fd67b14a50d15b5adf5dfd733d6e8d993e49050
taku8a <takuya0505hsmt@gmail.com>
2022-02-06 01:29:12 +0000

    [Add]genres_controllerのsearchアクションテスト

--- spec/requests/genres_spec.rb
+++ spec/requests/genres_spec.rb
@@ -149,4 +149,30 @@ RSpec.describe "genre_controllerのテスト", type: :request do
       end
     end
   end
+
+  describe 'GET searchアクションテスト' do
+    context 'ユーザーがログインしていない時' do
+      it '302レスポンスが返る' do
+        get search_genres_path
+        expect(response.status).to eq 302
+      end
+      it 'ログイン画面にリダイレクトされる' do
+        get search_genres_path
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ユーザーがログインしているとき' do
+      before do
+        sign_in @user
+        get search_genres_path,params: { content: @genre.name }
+      end
+
+      it '検索が成功する' do
+        expect(response.status).to eq 200
+        expect(response.body).to include 'ジャンル検索結果'
+        expect(response).to be_successful
+      end
+    end
+  end
 end
