780b9d8a0ff8bacd46c507694c9ab10f43958f2f
taku8a <takuya0505hsmt@gmail.com>
2022-02-09 00:13:15 +0000

    [Add]projects_controllerのshowアクションテスト

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -36,4 +36,33 @@ RSpec.describe "projects_controllerのテスト", type: :request do
       end
     end
   end
+
+  describe 'GET showアクションテスト' do
+
+    context 'ユーザーがログインしていない時' do
+      it '302レスポンスが返る' do
+        get project_path(@project)
+        expect(response.status).to eq 302
+      end
+      it 'ログイン画面にリダイレクトされる' do
+        get project_path(@project)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ユーザーがログインしているとき' do
+      before do
+        sign_in @user
+      end
+
+      it '正常に応答する' do
+        get project_path(@project)
+        expect(response).to be_successful
+      end
+      it '200レスポンスが返る' do
+        get project_path(@project)
+        expect(response.status).to eq 200
+      end
+    end
+  end
 end
