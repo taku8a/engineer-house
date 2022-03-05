2fefa0b11feff816e21c69b84bf98f3299cf374b
taku8a <takuya0505hsmt@gmail.com>
2022-03-05 15:51:59 +0000

    [Add]project_chats_controllerのindexアクションテスト

--- spec/requests/project_chats_spec.rb
+++ spec/requests/project_chats_spec.rb
@@ -1,10 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe "ProjectChats", type: :request do
-  describe "GET /project_chats" do
-    it "works! (now write some real specs)" do
-      get project_chats_index_path
-      expect(response).to have_http_status(200)
+RSpec.describe "project_chats_controllerのテスト", type: :request do
+
+  before do
+    @user = FactoryBot.create(:user)
+    @post = FactoryBot.create(:post, user:@user)
+    @genre = FactoryBot.create(:genre, owner_id: @user.id)
+    @project = FactoryBot.create(:project, owner_id: @user.id)
+    @project.users << @user
+    @project_chat = FactoryBot.create(:project_chat, user:@user, project:@project)
+  end
+
+  describe "GET indexアクションテスト" do
+    context 'ユーザーがログインしていない時' do
+      it '302レスポンスが返る' do
+        get project_project_chats_path(@project)
+        expect(response.status).to eq 302
+      end
+      it 'ログイン画面にリダイレクトされる' do
+        get project_project_chats_path(@project)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ユーザーがログインしているとき' do
+      before do
+        sign_in @user
+      end
+
+      it '正常に応答する' do
+        get project_project_chats_path(@project)
+        expect(response).to be_successful
+      end
+      it '200レスポンスが返る' do
+        get project_project_chats_path(@project)
+        expect(response.status).to eq 200
+      end
     end
   end
 end
