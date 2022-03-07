542ac4056586b631cac3c21d4666317fdb779099
taku8a <takuya0505hsmt@gmail.com>
2022-03-07 02:10:50 +0000

    [Add]genre_details_controllerのindexアクションテスト

--- spec/requests/genre_details_spec.rb
+++ spec/requests/genre_details_spec.rb
@@ -1,10 +1,43 @@
 require 'rails_helper'
 
-RSpec.describe "GenreDetails", type: :request do
-  describe "GET /genre_details" do
-    it "works! (now write some real specs)" do
-      get genre_details_path
-      expect(response).to have_http_status(200)
+RSpec.describe "genre_details_controllerのテスト", type: :request do
+
+  before do
+    @user = FactoryBot.create(:user)
+    @post = FactoryBot.create(:post, user:@user)
+    @post_comment = FactoryBot.create(:post_comment, user:@user, post:@post)
+    @genre = FactoryBot.create(:genre, owner_id: @user.id)
+    @genre_detail = FactoryBot.create(:genre_detail, genre:@genre)
+    @project = FactoryBot.create(:project, owner_id: @user.id)
+    @project.users << @user
+    @project_chat = FactoryBot.create(:project_chat, user:@user, project:@project)
+  end
+
+  describe "GET indexアクションテスト" do
+    context 'ユーザーがログインしていない時' do
+      it '302レスポンスが返る' do
+        get genre_genre_details_path(@genre)
+        expect(response.status).to eq 302
+      end
+      it 'ログイン画面にリダイレクトされる' do
+        get genre_genre_details_path(@genre)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ユーザーがログインしているとき' do
+      before do
+        sign_in @user
+      end
+
+      it '正常に応答する' do
+        get genre_genre_details_path(@genre)
+        expect(response).to be_successful
+      end
+      it '200レスポンスが返る' do
+        get genre_genre_details_path(@genre)
+        expect(response.status).to eq 200
+      end
     end
   end
 end
