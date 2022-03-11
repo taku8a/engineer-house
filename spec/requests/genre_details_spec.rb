972308fb6d258df0c530270216c0ed3a27ec488a
taku8a <takuya0505hsmt@gmail.com>
2022-03-11 02:08:18 +0000

    [Add]genre_details_controllerのcreateアクションテスト

--- spec/requests/genre_details_spec.rb
+++ spec/requests/genre_details_spec.rb
@@ -125,4 +125,32 @@ RSpec.describe "genre_details_controllerのテスト", type: :request do
       end
     end
   end
+
+  describe 'POST createアクションテスト' do
+    context 'ユーザーがログインしていない時' do
+      it '302レスポンスが返る' do
+        post genre_genre_details_path(@genre)
+        expect(response.status).to eq 302
+      end
+      it 'ログイン画面にリダイレクトされる' do
+        post genre_genre_details_path(@genre)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ユーザーがログインしているとき' do
+      before do
+        sign_in @user
+      end
+
+      it '新規投稿する' do
+        genre_detail_params = FactoryBot.attributes_for(:genre_detail, genre: @genre)
+        post genre_genre_details_path(@genre), params: { genre_detail: genre_detail_params }
+        get genre_genre_detail_path(@genre_detail.genre_id,@genre_detail.id)
+        expect(:notice).to be_present
+        expect(response).to be_successful
+        expect(response.status).to eq 200
+      end
+    end
+  end
 end
